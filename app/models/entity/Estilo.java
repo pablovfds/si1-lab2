a42df0be57120fe0f2bd9b1ea37caf1fd65efbdc
Páblo Victor <pablo.santos@ccc.ufcg.edu.br>
2015-06-04 13:47:29 -0300

    Refatoramento do codigo

--- app/models/entity/Estilo.java
+++ app/models/entity/Estilo.java
@@ -6,37 +6,37 @@ import javax.persistence.*;
 
 @Entity(name = "estilo")
 public class Estilo implements Serializable {
-	
-	@Id
-	@GeneratedValue
-	private long id;
-	
-	@Column(name = "Nome", nullable = false)
-	private String nome;
 
-	@Transient
-	private static final long serialVersionUID = 1L;
-	
-	public Estilo() {
-	}
+    @Id
+    @GeneratedValue
+    private long id;
 
-	public Estilo(String nome) {
-		this.nome = nome;
-	}
+    @Column(name = "nomeDoEstilo", nullable = false)
+    private String nomeDoEstilo;
 
-	public long getId() {
-		return id;
-	}
+    @Transient
+    private static final long serialVersionUID = 1L;
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public Estilo() {
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Estilo(String nomeDoEstilo) {
+        this.nomeDoEstilo = nomeDoEstilo;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomeDoEstilo() {
+        return nomeDoEstilo;
+    }
+
+    public void setNomeDoEstilo(String nome) {
+        this.nomeDoEstilo = nome;
+    }
 }
