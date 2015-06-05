7cbb1678ea4b32be08c9ef563143659709c6d6f9
Páblo Victor <pablo.santos@ccc.ufcg.edu.br>
2015-06-05 14:58:16 -0300

    Testes do diretorio Entity terminados

--- test/modelsTest/anuncioTest.java
+++ test/modelsTest/anuncioTest.java
@@ -1,84 +1,207 @@
 package modelsTest;
 
-import models.Anuncio;
+import models.entity.Anuncio;
+import models.entity.Estilo;
+import models.entity.Instrumento;
 import org.junit.*;
 
-/**
- * Created by PabloVictor on 23/05/2015.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class anuncioTest {
-    Anuncio anuncio;
+    private Anuncio anuncio;
+    private String titulo = "Titulo Teste";
+    private String descricao = "Descrição Teste";
+    private String codigo = "12345";
+    private String perfilDoFacebook = "https://www.facebook.com/contateste123";
+    private String email = "contato@contato.com";
+    private String buscaPor = "formar banda";
+    private List<Instrumento> lista = new ArrayList<>();
+    private List<Estilo> listaEstilosLike = new ArrayList<>();
+    private Estilo estilo1;
+    private Estilo estilo2;
+    private List<Estilo> listaEstilosNotLike = new ArrayList<>();
+    private Instrumento instrument;
+    private String bairro = "Bairro Azul";
+    private String cidade = "Cidade Azul";
 
-    @Test
-    public void testaListaDeContatos(){
+    @Before
+    public void criaAnuncio(){
         anuncio = new Anuncio();
-        Assert.assertEquals("Campo não Preenchido", anuncio.getListaParaContato()[0]);
-        Assert.assertEquals("Campo não Preenchido", anuncio.getListaParaContato()[1]);
-
         try {
-            anuncio.setContato(null, null);
+            estilo1 = new Estilo("Samba");
         } catch (Exception e) {
-            Assert.assertEquals("Preencha ao menos um campo para contato", e.getMessage());
+            e.printStackTrace();
+        }
+        try {
+            estilo2 = new Estilo("Rock");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         try {
-            anuncio.setContato("contato@contato.com", null);
-            Assert.assertEquals("contato@contato.com", anuncio.getListaParaContato()[0]);
+            instrument = new Instrumento("Baixo");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testaTitulo(){
+        try {
+            anuncio.setTitulo(null);
+        } catch (Exception e) {
+            Assert.assertEquals("Preencha o campo: Titulo", e.getMessage());
+        }
+
+        try {
+            anuncio.setTitulo("");
+        } catch (Exception e) {
+            Assert.assertEquals("Preencha o campo: Titulo", e.getMessage());
+        }
+
+        try {
+            anuncio.setTitulo(titulo);
+            Assert.assertTrue(titulo.toUpperCase().equals(anuncio.getTitulo()));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testaDescricao(){
+        try {
+            anuncio.setDescricao(null);
+        } catch (Exception e) {
+            Assert.assertEquals("Preencha o campo: Descricao", e.getMessage());
+        }
+
+        try {
+            anuncio.setDescricao("");
+        } catch (Exception e) {
+            Assert.assertEquals("Preencha o campo: Descricao", e.getMessage());
+        }
+
+        try {
+            anuncio.setDescricao(descricao);
+            Assert.assertTrue(descricao.toUpperCase().equals(anuncio.getDescricao()));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testaCodigoDoAnuncio(){
+        try {
+            anuncio.setCodigoDoAnuncio(null);
+        } catch (Exception e) {
+            Assert.assertEquals("Preencha o campo: Codigo do anuncio", e.getMessage());
+        }
+
+        try {
+            anuncio.setCodigoDoAnuncio("");
+        } catch (Exception e) {
+            Assert.assertEquals("Preencha o campo: Codigo do anuncio", e.getMessage());
+        }
+
+        try {
+            anuncio.setCodigoDoAnuncio("codigoTeste");
+        } catch (Exception e) {
+            Assert.assertEquals("Seu codigo do anuncio deve ter no maximo 5 caracteres.", e.getMessage());
+        }
+
+        try {
+            anuncio.setCodigoDoAnuncio(codigo);
+
+            Assert.assertTrue(codigo.equals(anuncio.getCodigoDoAnuncio()));
         } catch (Exception e1) {
             e1.printStackTrace();
         }
 
+    }
+
+    @Test
+    public void testaPerfilDoFacebook(){
+
         try {
-            anuncio.setContato(null, "www.facebook.com/contato");
-            Assert.assertEquals("www.facebook.com/contato", anuncio.getListaParaContato()[1]);
+            anuncio.setPerfilDoFacebook(null);
         } catch (Exception e) {
-            e.printStackTrace();
+            Assert.assertEquals("Preencha o campo: Perfil do Facebook", e.getMessage());
         }
 
         try {
-            anuncio.setContato("contato@contato.com", "www.facebook.com/contato");
-            Assert.assertEquals("contato@contato.com", anuncio.getListaParaContato()[0]);
-            Assert.assertEquals("www.facebook.com/contato", anuncio.getListaParaContato()[1]);
+            anuncio.setPerfilDoFacebook("");
         } catch (Exception e) {
-            e.printStackTrace();
+            Assert.assertEquals("Preencha o campo: Perfil do Facebook", e.getMessage());
+        }
+
+        try {
+            anuncio.setPerfilDoFacebook(perfilDoFacebook);
+            Assert.assertTrue(perfilDoFacebook.equals(anuncio.getPerfilDoFacebook()));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testaEmail(){
+
+        Assert.assertEquals(null, anuncio.getEmail());
+
+        try {
+            anuncio.setEmail(null);
+        } catch (Exception e) {
+            Assert.assertTrue("Preencha o campo: E-mail".equals(e.getMessage()));
+        }
+
+        try {
+            anuncio.setEmail("");
+        } catch (Exception e) {
+            Assert.assertTrue("Preencha o campo: E-mail".equals(e.getMessage()));
+        }
+
+        try {
+            anuncio.setEmail("emailinvalido");
+        } catch (Exception e) {
+            Assert.assertTrue("Preencha o campo: E-mail".equals(e.getMessage()));
+        }
+
+        try {
+            anuncio.setEmail(email);
+            Assert.assertTrue(email.equals(anuncio.getEmail()));
+        } catch (Exception e1) {
+            e1.printStackTrace();
         }
     }
 
     @Test
     public void testaOpcaoProcuraBanda(){
-        anuncio = new Anuncio();
-        Assert.assertEquals(null, anuncio.getOpcaoProcuraBanda());
+        Assert.assertEquals(null, anuncio.getBuscaPor());
 
         try {
-            anuncio.setOpcaoProcuraBanda(null);
+            anuncio.setBuscaPor(null);
         } catch (Exception e) {
-            Assert.assertEquals("Campo não preenchido: Procuro uma banda", e.getMessage());
+            Assert.assertTrue("Preencha o campo: Busca por".equals(e.getMessage()));
         }
 
         try {
-            anuncio.setOpcaoProcuraBanda("Sim");
-            Assert.assertEquals("SIM", anuncio.getOpcaoProcuraBanda());
+            anuncio.setBuscaPor("");
+        } catch (Exception e) {
+            Assert.assertTrue("Preencha o campo: Busca por".equals(e.getMessage()));
+        }
+
+        try {
+            anuncio.setBuscaPor(buscaPor);
+            Assert.assertEquals(buscaPor.toUpperCase(), anuncio.getBuscaPor());
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Test
-    public void testaOpcaoDeTocarOcasionalmente(){
-        anuncio = new Anuncio();
-
-        Assert.assertEquals(null, anuncio.getOpcaoDeTocarOcasionalmente());
 
         try {
-            anuncio.setOpcaoDeTocarOcasionalmente(null);
-        } catch (Exception e) {
-            Assert.assertEquals("Campo não preenchido: Tocar ocasionalmente", e.getMessage());
-        }
-
-
-        try {
-            anuncio.setOpcaoDeTocarOcasionalmente("Sim");
-            Assert.assertEquals("SIM", anuncio.getOpcaoDeTocarOcasionalmente());
+            String buscaPor1 = "tocar ocasionalmente";
+            anuncio.setBuscaPor(buscaPor1);
+            Assert.assertEquals(buscaPor1.toUpperCase(), anuncio.getBuscaPor());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -86,47 +209,51 @@ public class anuncioTest {
 
     @Test
     public void testaEstilosQueAnuncianteNaoGosta(){
-        anuncio = new Anuncio();
 
-        Assert.assertTrue(anuncio.getEstilosQueAnuncianteNaoGosta().size() == 0);
+        Assert.assertTrue(anuncio.getEstilosQueNaoGosta().size() == 0);
 
         try {
-            anuncio.setEstilosQueAnuncianteNaoGosta("Rock");
+            listaEstilosNotLike.add(estilo2);
+            anuncio.setEstilosQueNaoGosta(listaEstilosNotLike);
 
-            Assert.assertTrue(anuncio.getEstilosQueAnuncianteNaoGosta().size() == 1);
-            Assert.assertEquals("ROCK", anuncio.getEstilosQueAnuncianteNaoGosta().get(0));
+            Assert.assertTrue(anuncio.getEstilosQueNaoGosta().size() == 1);
+            Assert.assertEquals(estilo2, anuncio.getEstilosQueNaoGosta().get(0));
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
-            anuncio.setEstilosQueAnuncianteNaoGosta("samba");
+            listaEstilosNotLike.add(estilo1);
+            anuncio.setEstilosQueNaoGosta(listaEstilosNotLike);
 
-            Assert.assertTrue(anuncio.getEstilosQueAnuncianteNaoGosta().size() == 2);
-            Assert.assertEquals("SAMBA", anuncio.getEstilosQueAnuncianteNaoGosta().get(1));
+            Assert.assertTrue(anuncio.getEstilosQueNaoGosta().size() == 2);
+            Assert.assertEquals(estilo1, anuncio.getEstilosQueNaoGosta().get(1));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
     @Test
     public void testaEstilosQueAnuncianteGosta(){
-        anuncio = new Anuncio();
-        Assert.assertTrue(anuncio.getEstilosQueAnuncianteGosta().size() == 0);
+
+        Assert.assertTrue(anuncio.getEstilosQueGosta().size() == 0);
 
         try {
-            anuncio.setEstilosQueAnuncianteGosta("Rock");
 
-            Assert.assertTrue(anuncio.getEstilosQueAnuncianteGosta().size() == 1);
-            Assert.assertEquals("ROCK", anuncio.getEstilosQueAnuncianteGosta().get(0));
+            listaEstilosLike.add(estilo2);
+            anuncio.setEstilosQueGosta(listaEstilosLike);
+
+            Assert.assertTrue(anuncio.getEstilosQueGosta().size() == 1);
+            Assert.assertEquals(estilo2, anuncio.getEstilosQueGosta().get(0));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try {
-            anuncio.setEstilosQueAnuncianteNaoGosta("samba");
 
-            Assert.assertTrue(anuncio.getEstilosQueAnuncianteGosta().size() == 2);
-            Assert.assertEquals("SAMBA", anuncio.getEstilosQueAnuncianteGosta().get(1));
+        try {
+            listaEstilosLike.add(estilo1);
+            anuncio.setEstilosQueGosta(listaEstilosLike);
+
+            Assert.assertTrue(anuncio.getEstilosQueGosta().size() == 2);
+            Assert.assertEquals(estilo1, anuncio.getEstilosQueGosta().get(1));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -134,25 +261,19 @@ public class anuncioTest {
 
     @Test
     public void testaInstrumentosQueOAnuncianteToca(){
-        anuncio = new Anuncio();
 
-        Assert.assertTrue(anuncio.getListaDeInstrumentosQueOAnuncianteToca().size() == 0);
+        Assert.assertTrue(anuncio.getInstrumentos().size() == 0);
         try {
-            anuncio.setInstrumentosQueOAnuncianteToca("");
+            anuncio.setInstrumentos(null);
         } catch (Exception e) {
-            Assert.assertEquals("Campo não preenchido: Instrumentos que o anunciante toca", e.getMessage());
+            Assert.assertEquals("Preencha o campo: Instrumento que toco", e.getMessage());
         }
-        Assert.assertTrue(anuncio.getListaDeInstrumentosQueOAnuncianteToca().size() == 0);
+        Assert.assertTrue(anuncio.getInstrumentos().size() == 0);
         try {
-            anuncio.setInstrumentosQueOAnuncianteToca(null);
-        } catch (Exception e) {
-            Assert.assertEquals("Campo não preenchido: Instrumentos que o anunciante toca", e.getMessage());
-        }
-        Assert.assertTrue(anuncio.getListaDeInstrumentosQueOAnuncianteToca().size() == 0);
-        try {
-            anuncio.setInstrumentosQueOAnuncianteToca("Baixo");
-            Assert.assertTrue(anuncio.getListaDeInstrumentosQueOAnuncianteToca().size() == 1);
-            Assert.assertEquals("BAIXO", anuncio.getListaDeInstrumentosQueOAnuncianteToca().get(0));
+            lista.add(instrument);
+            anuncio.setInstrumentos(lista);
+            Assert.assertTrue(anuncio.getInstrumentos().size() == 1);
+            Assert.assertEquals(instrument, anuncio.getInstrumentos().get(0));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -160,25 +281,22 @@ public class anuncioTest {
 
     @Test
     public void testaBairroDoAnunciante(){
-        anuncio = new Anuncio();
-        Assert.assertEquals(null, anuncio.getBairroDoAnunciante());
 
         try {
-            anuncio.setBairroDoAnunciante("");
+            anuncio.setBairro("");
         } catch (Exception e) {
-            Assert.assertEquals("Campo não preenchido: bairro do anunciante", e.getMessage());
+            Assert.assertEquals("Preencha o campo: Bairro", e.getMessage());
         }
-        Assert.assertEquals(null, anuncio.getBairroDoAnunciante());
-        try {
-            anuncio.setBairroDoAnunciante(null);
-        } catch (Exception e) {
-            Assert.assertEquals("Campo não preenchido: bairro do anunciante", e.getMessage());
-        }
-        Assert.assertEquals(null, anuncio.getBairroDoAnunciante());
 
         try {
-            anuncio.setBairroDoAnunciante("Bairro Azul");
-            Assert.assertEquals("BAIRRO AZUL", anuncio.getBairroDoAnunciante());
+            anuncio.setBairro(null);
+        } catch (Exception e) {
+            Assert.assertEquals("Preencha o campo: Bairro", e.getMessage());
+        }
+
+        try {
+            anuncio.setBairro(bairro);
+            Assert.assertEquals(bairro.toUpperCase(), anuncio.getBairro());
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -186,27 +304,96 @@ public class anuncioTest {
 
     @Test
     public void testaCidadeDoAnunciante(){
-        anuncio = new Anuncio();
-        Assert.assertEquals(null, anuncio.getCidadeDoAnunciante());
+
+        Assert.assertEquals(null, anuncio.getCidade());
 
         try {
-            anuncio.setBairroDoAnunciante("");
+            anuncio.setCidade("");
         } catch (Exception e) {
-            Assert.assertEquals("Campo não foi preenchido: Cidade do anunciante", e.getMessage());
+            Assert.assertEquals("Preencha o campo: Cidade", e.getMessage());
         }
-        Assert.assertEquals(null, anuncio.getCidadeDoAnunciante());
+        Assert.assertEquals(null, anuncio.getCidade());
         try {
-            anuncio.setBairroDoAnunciante(null);
+            anuncio.setCidade(null);
         } catch (Exception e) {
-            Assert.assertEquals("Campo não foi preenchido: Cidade do anunciante", e.getMessage());
+            Assert.assertEquals("Preencha o campo: Cidade", e.getMessage());
         }
-        Assert.assertEquals(null, anuncio.getCidadeDoAnunciante());
+        Assert.assertEquals(null, anuncio.getCidade());
 
         try {
-            anuncio.setBairroDoAnunciante("Cidade Azul");
-            Assert.assertEquals("CIDADE AZUL", anuncio.getCidadeDoAnunciante());
+            anuncio.setCidade(cidade);
+            Assert.assertEquals("CIDADE AZUL", anuncio.getCidade());
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testaCriacao(){
+        try {
+            anuncio = new Anuncio(titulo, descricao, codigo, cidade, bairro,
+                    email, perfilDoFacebook, buscaPor, lista,
+                    listaEstilosLike, listaEstilosNotLike);
+            Assert.assertEquals(titulo.toUpperCase(), anuncio.getTitulo());
+            Assert.assertEquals(descricao.toUpperCase(), anuncio.getDescricao());
+            Assert.assertEquals(buscaPor.toUpperCase(), anuncio.getBuscaPor());
+            Assert.assertEquals(codigo, anuncio.getCodigoDoAnuncio());
+            Assert.assertEquals(cidade.toUpperCase(), anuncio.getCidade());
+            Assert.assertEquals(bairro.toUpperCase(), anuncio.getBairro());
+            Assert.assertEquals(email, anuncio.getEmail());
+            Assert.assertEquals(perfilDoFacebook, anuncio.getPerfilDoFacebook());
+            Assert.assertEquals(lista, anuncio.getInstrumentos());
+            Assert.assertEquals(listaEstilosLike, anuncio.getEstilosQueGosta());
+            Assert.assertEquals(listaEstilosNotLike, anuncio.getEstilosQueNaoGosta());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testaEquals(){
+        Anuncio anuncio1;
+        try {
+            anuncio1 = new Anuncio("outroTitulo", descricao, codigo, cidade, bairro,
+                    email, perfilDoFacebook, buscaPor, lista,
+                    listaEstilosLike, listaEstilosNotLike);
+            anuncio = new Anuncio(titulo, descricao, codigo, cidade, bairro,
+                    email, perfilDoFacebook, buscaPor, lista,
+                    listaEstilosLike, listaEstilosNotLike);
+
+            Assert.assertFalse(anuncio.equals(anuncio1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            anuncio1 = new Anuncio(titulo, descricao, "54321", cidade, bairro,
+                    email, perfilDoFacebook, buscaPor, lista,
+                    listaEstilosLike, listaEstilosNotLike);
+            anuncio = new Anuncio(titulo, descricao, codigo, cidade, bairro,
+                    email, perfilDoFacebook, buscaPor, lista,
+                    listaEstilosLike, listaEstilosNotLike);
+
+            Assert.assertFalse(anuncio.equals(anuncio1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            anuncio1 = new Anuncio(titulo, descricao, codigo, cidade, bairro,
+                    email, perfilDoFacebook, buscaPor, lista,
+                    listaEstilosLike, listaEstilosNotLike);
+            anuncio = new Anuncio(titulo, descricao, codigo, cidade, bairro,
+                    email, perfilDoFacebook, buscaPor, lista,
+                    listaEstilosLike, listaEstilosNotLike);
+
+            Assert.assertTrue(anuncio.equals(anuncio1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
