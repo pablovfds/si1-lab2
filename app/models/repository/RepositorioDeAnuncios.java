cbf501b7ba0396631e6e5bc304fbf6a981542554
Páblo Victor <pablo.santos@ccc.ufcg.edu.br>
2015-06-05 15:40:32 -0300

    Retirado o metodo de pesquisa

--- app/models/repository/RepositorioDeAnuncios.java
+++ app/models/repository/RepositorioDeAnuncios.java
@@ -10,9 +10,7 @@ import javax.persistence.Query;
 public class RepositorioDeAnuncios extends GenericRepository<Anuncio> {
 	
 	private static RepositorioDeAnuncios instance;
-	private List<Anuncio> listaResultado;
-	private List<String> listaDeAssuntos;
-	private List<Anuncio> listaDeAnuncios;
+
 	private RepositorioDeAnuncios() {
 		super(Anuncio.class);
 	}
@@ -23,27 +21,5 @@ public class RepositorioDeAnuncios extends GenericRepository<Anuncio> {
 		}
 
 		return instance;
-	}
-
-	public List<Anuncio> findByAttributeName(String attributeValue) {
-		listaResultado = new ArrayList<>();
-		listaDeAssuntos = new ArrayList<>();
-
-		listaDeAssuntos.add("titulo");
-		listaDeAssuntos.add("descricao");
-		listaDeAssuntos.add("buscaPor");
-		listaDeAssuntos.add("estilosQueGosta");
-		listaDeAssuntos.add("estilosQueNaoGosta");
-
-		for (String assunto : listaDeAssuntos){
-			listaDeAnuncios = instance.findByAttributeName(assunto, attributeValue);
-			for (Anuncio anuncio : listaDeAnuncios){
-				if	(!listaResultado.contains(anuncio)){
-					listaResultado.add(anuncio);
-				}
-			}
-		}
-
-		return listaResultado;
 	}
 }
